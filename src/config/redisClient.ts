88433afb525c372eacc8d72b93df8e7f50627278
Chandu8817 <virendrakapoor45@gmail.com>
2025-09-23 17:54:46 +0530

    max try issue

--- src/config/redisClient.ts
+++ src/config/redisClient.ts
@@ -2,8 +2,16 @@ import Redis from "ioredis";
 import dotenv from "dotenv";
 dotenv.config();
 
-// Connect to Redis (default port 6379)
-export const redis = new Redis(process.env.REDIS_URL!);
+
+export const redis = new Redis(process.env.REDIS_URL!, {
+  maxRetriesPerRequest: null, 
+  enableReadyCheck: false, 
+  retryStrategy: (times) => {
+    if (times > 10) return null; 
+    return Math.min(times * 200, 2000); 
+  },
+});
+
 
 redis.on("connect", () => {
   console.log("✅ Connected to Redis");
