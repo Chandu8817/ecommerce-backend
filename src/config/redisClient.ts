1f3b9070c23fd720c4f4da6bef699b568d8ab87d
Chandu8817 <virendrakapoor45@gmail.com>
2025-09-23 12:55:51 +0530

    fix redis

--- src/config/redisClient.ts
+++ src/config/redisClient.ts
@@ -1,11 +1,9 @@
 import Redis from "ioredis";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Connect to Redis (default port 6379)
-export const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379,
-  // password: "yourpassword" // only if Redis is password protected
-});
+export const redis = new Redis(process.env.REDIS_URL!);
 
 redis.on("connect", () => {
   console.log("✅ Connected to Redis");
