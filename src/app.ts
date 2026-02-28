81605d49e092ab7630720871398fb574364c070a
Chandu8817 <virendrakapoor45@gmail.com>
2026-02-28 13:47:40 +0530

    add dmomain

--- src/app.ts
+++ src/app.ts
@@ -21,6 +21,7 @@ const app = express()
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   "https://www.rawbharat.shop",
+  "https://rawbharat.shop/",
   "https://store-manager-rawbharat.netlify.app",
   "http://localhost:8080",
   "http://localhost:5173",
